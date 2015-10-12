7b9765ec2b25eab390fe91961b0570e52a5a8235
su2278370 <su2278370@gmail.com>
2015-10-12 23:05:38 +0800

    Change struct

--- phonebook_opt.h
+++ phonebook_opt.h
@@ -5,7 +5,15 @@
 
 /* original version */
 typedef struct __PHONE_BOOK_ENTRY {
+
     char lastName[MAX_LAST_NAME_SIZE];
+    struct __PHONE_BOOK_ENTRY *pNext;
+    struct __PHONE_BOOK_DETAIL *pDetail;
+
+} entry;
+
+typedef struct __PHONE_BOOK_DETAIL {
+
     char firstName[16];
     char email[16];
     char phone[10];
@@ -15,8 +23,8 @@ typedef struct __PHONE_BOOK_ENTRY {
     char city[16];
     char state[2];
     char zip[5];
-    struct __PHONE_BOOK_ENTRY *pNext;
-} entry;
+
+} detail;
 
 entry *findName(char lastname[], entry *pHead);
 entry *append(char lastName[], entry *e);
